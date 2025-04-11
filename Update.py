81ad9c68ab11fb9b7fa2cced99ec87d769d8bd28
Malveillance3Dev <slowa0678@gmail.com>
2025-04-11 16:29:38 +0200

    Update.py
    assure you have replaced path\to\your\gitclone\of\lolipop, by the real clone of tool.

---
+++ Update.py
@@ -0,0 +1,27 @@
+import git
+import os
+import sys
+
+repo_dir = 'path\to\your\gitclone\of\Lolipop'  
+
+def update_repo():
+    try:
+        if not os.path.isdir(repo_dir):
+            print("Repository directory doesn't exist. Please clone the repository.")
+            return
+
+        repo = git.Repo(repo_dir)
+        origin = repo.remotes.origin
+        origin.fetch()
+
+        branch = repo.head.ref
+        repo.git.merge('origin/' + branch)
+
+        print("Repository has been successfully updated!")
+
+    except git.exc.GitCommandError as e:
+        print(f"Git error: {e}")
+        sys.exit(1)
+
+if __name__ == '__main__':
+    update_repo()
