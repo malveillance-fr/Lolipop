70f49a12f0dca4b19dd2814a6737c392ac68f709
Malveillance3Dev <slowa0678@gmail.com>
2025-04-12 11:24:18 +0200

    start_debug.bat

--- LOLIPOP/start_debug.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-python Lolipop.py
-if %errorlevel% neq 0 (
-    echo The tool failed to start due to a "TRACEBACK" error. We recommend reinstalling the tool.
-    echo We apologize for this crash.
-    echo.
-    echo Github: https://github.com/malveillance-fr/Lolipop
-)
