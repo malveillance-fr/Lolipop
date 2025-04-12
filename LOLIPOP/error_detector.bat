7f66a45263f093823bfd28f0e0384ef4e24d81b5
Malveillance3Dev <slowa0678@gmail.com>
2025-04-12 16:01:11 +0200

    error_detector.bat

--- LOLIPOP/error_detector.bat
+++ LOLIPOP/error_detector.bat
@@ -2,7 +2,9 @@
 python Lolipop.py
 if %errorlevel% neq 0 (
     echo The tool failed to start due to a "TRACEBACK" error. We recommend reinstalling the tool.
-    echo We apologize for this crash.
+    echo We apologize for this crash.                         
     echo.
     echo Github: https://github.com/malveillance-fr/Lolipop
 )
+
+:: If the "error_detector" launches Lolipop normally, it means that there are no errors in the tool
